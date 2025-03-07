5130baca1c1e60197bd3222ed14cd33089f6d765
Himanshu Menghani <himanshumenghani524@gmail.com>
2025-03-07 19:07:20 +0530

    update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from routes.symptoms import symptoms_bp
 from routes.reports import reports_bp
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ app.register_blueprint(symptoms_bp, url_prefix='/api/symptoms')
 app.register_blueprint(reports_bp, url_prefix='/api/report')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use Render's port or default to 5000
+    app.run(host='0.0.0.0', port=port)
